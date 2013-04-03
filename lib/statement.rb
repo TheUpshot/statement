984b02c2d47240e4c667ac873fc39cb219fb0d2a
Derek Willis <dwillis@gmail.com>
2013-04-03 13:06:07 -0400

    add rss parser

--- lib/statement.rb
+++ lib/statement.rb
@@ -8,6 +8,12 @@ module Statement
   
   class Link
     
+    def self.from_rss(url)
+      doc = Nokogiri::XML(open(url))
+      links = doc.xpath('//item')
+      links.map{|link| {:source => url, :url => link.xpath('link').text, :title => link.xpath('title').text, :date => Date.parse(link.xpath('pubDate').text), :domain => URI.parse(link.xpath('link').text).host }}
+    end
+    
     def self.house_gop(url)
       uri = URI.parse(url)
       date = Date.parse(uri.query.split('=').last)
