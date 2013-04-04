808cc001d5946fef773c5b07edd6b38d03296d06
Derek Willis <dwillis@gmail.com>
2013-04-04 12:23:16 -0400

    added initial tests

---
+++ spec/statement_spec.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require_relative "../lib/statement"
+
+describe Statement do
+  it "parses an rss feed" do
+    @results = Statement::Link.from_rss("http://ruiz.house.gov/rss.xml")
+    @results.first[:domain].must_equal "ruiz.house.gov"
+  end
+  
+  it "parses House GOP press release page" do
+    @results = Statement::Link.house_gop("http://www.gop.gov/republicans/news?offset=03/29/13")
+    @results.first[:source].must_equal "http://www.gop.gov/republicans/news?offset=03/29/13"
+  end
+  
+end
