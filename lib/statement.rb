151f57866399197d4fff51bd735b09b96ea4baf6
Jacob Harris <jharris@nytimes.com>
2013-04-22 12:39:58 -0400

    Added some code to make relative URLs absolute

--- lib/statement.rb
+++ lib/statement.rb
@@ -7,11 +7,19 @@ require 'nokogiri'
 module Statement
   
   class Link
-    
+    def self.absolute_link(url, link)
+      return link if link =~ /^http:\/\//
+
+      (URI.parse(url) + link).to_s
+    end
+
     def self.from_rss(url)
       doc = Nokogiri::XML(open(url))
       links = doc.xpath('//item')
-      links.map{|link| {:source => url, :url => link.xpath('link').text, :title => link.xpath('title').text, :date => link.xpath('pubDate').empty? ? nil: Date.parse(link.xpath('pubDate').text), :domain => URI.parse(link.xpath('link').text).host }}
+      links.map do |link| 
+        abs_link = absolute_link(url, link.xpath('link').text)
+        { :source => url, :url => abs_link, :title => link.xpath('title').text, :date => link.xpath('pubDate').empty? ? nil: Date.parse(link.xpath('pubDate').text), :domain => URI.parse(link.xpath('link').text).host }
+      end
     end
     
     def self.house_gop(url)
@@ -19,7 +27,11 @@ module Statement
       date = Date.parse(uri.query.split('=').last)
       doc = Nokogiri::HTML(open(url).read)
       links = doc.xpath("//ul[@id='membernews']").search('a')
-      links.map{|link| {:source => url, :url => link["href"], :title => link.text.strip, :date => date, :domain => URI.parse(link["href"]).host}}
+      links.map do |link| 
+        # return a hash
+        abs_link = absolute_link(url, link["href"])
+        { :source => url, :url => abs_link, :title => link.text.strip, :date => date, :domain => URI.parse(link["href"]).host }
+      end
     end
     
   end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -24,5 +24,11 @@ describe Statement do
     @results = Statement::Link.from_rss(@feed_url)
     @results.first[:date].must_equal nil
   end
-  
+
+  it "handles relative URLs" do
+    @feed_url = "http://www.gop.gov/republicans/news?offset=03/29/13"
+    stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "house_gop_releases.html")), :status => 200)
+    @results = Statement::Link.house_gop(@feed_url)
+    @results.last[:url].must_equal "http://www.gop.gov/republicans/other/relative_url_test.html"
+  end
 end
