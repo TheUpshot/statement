e8482ef8fd44c24be9bf5d08c356787e41664ce5
Jacob Harris <jharris@nytimes.com>
2013-04-22 10:32:23 -0400

    Switch tests to use webmock

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,19 +1,27 @@
 require "minitest/autorun"
 require_relative "../lib/statement"
+require 'webmock/minitest'
 
 describe Statement do
   it "parses an rss feed" do
-    @results = Statement::Link.from_rss("http://ruiz.house.gov/rss.xml")
+    @feed_url = "http://ruiz.house.gov/rss.xml"
+    stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "ruiz_rss.xml")), :status => 200)
+    @results = Statement::Link.from_rss(@feed_url)
     @results.first[:domain].must_equal "ruiz.house.gov"
   end
   
   it "parses House GOP press release page" do
-    @results = Statement::Link.house_gop("http://www.gop.gov/republicans/news?offset=03/29/13")
-    @results.first[:source].must_equal "http://www.gop.gov/republicans/news?offset=03/29/13"
+    @feed_url = "http://www.gop.gov/republicans/news?offset=03/29/13"
+    stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "house_gop_releases.html")), :status => 200)
+    @results = Statement::Link.house_gop(@feed_url)
+    @results.first[:source].must_equal @feed_url
   end
   
   it "does not attempt to parse dates when none are present" do
-    @results = Statement::Link.from_rss("http://culberson.house.gov/feed/rss/")
+    @feed_url = "http://culberson.house.gov/feed/rss/"
+    stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "culberson_rss.xml")), :status => 200)
+
+    @results = Statement::Link.from_rss(@feed_url)
     @results.first[:date].must_equal nil
   end
   
--- statement.gemspec
+++ statement.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency 'webmock'
   spec.add_dependency "american_date"
   spec.add_dependency "nokogiri"
 end
