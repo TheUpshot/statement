bfb1db354b6f50db4ee3ce7f3472fe646703a33f
Derek Willis <dwillis@gmail.com>
2013-05-07 15:00:18 -0400

    fixed Date parser for empty dates

--- lib/statement.rb
+++ lib/statement.rb
@@ -30,7 +30,7 @@ module Statement
     end
     
     def self.date_from_rss_item(link)
-      if !link.xpath('pubDate').empty?
+      if !link.xpath('pubDate').text.blank?
         Date.parse(link.xpath('pubDate').text)
       elsif !link.xpath('pubdate').empty?
         Date.parse(link.xpath('pubdate').text)
