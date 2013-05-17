8c040b9c28f5240840eb98b20f75fe50497f7654
Derek Willis <dwillis@gmail.com>
2013-05-17 16:31:29 -0400

    split statement into classes

---
+++ lib/statement/utils.rb
@@ -0,0 +1,16 @@
+require 'uri'
+
+module Statement
+  class Utils
+    
+    def self.absolute_link(url, link)
+      return link if link =~ /^http:\/\//
+      ("http://"+URI.parse(url).host + "/"+link).to_s
+    end
+
+    def self.remove_generic_urls!(results)
+      results.reject{|r| URI.parse(URI.escape(r[:url])).path == '/news/' or URI.parse(URI.escape(r[:url])).path == '/news'}
+    end
+    
+  end
+end
