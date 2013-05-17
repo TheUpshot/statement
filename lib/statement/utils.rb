2ecdcf184eedc86910d58d5a044c129600105ce3
Derek Willis <dwillis@gmail.com>
2013-05-17 16:42:02 -0400

    made utils a module

--- lib/statement/utils.rb
+++ lib/statement/utils.rb
@@ -1,16 +1,12 @@
 require 'uri'
 
-module Statement
-  class Utils
-    
-    def self.absolute_link(url, link)
-      return link if link =~ /^http:\/\//
-      ("http://"+URI.parse(url).host + "/"+link).to_s
-    end
+module Utils
+  def self.absolute_link(url, link)
+    return link if link =~ /^http:\/\//
+    ("http://"+URI.parse(url).host + "/"+link).to_s
+  end
 
-    def self.remove_generic_urls!(results)
-      results.reject{|r| URI.parse(URI.escape(r[:url])).path == '/news/' or URI.parse(URI.escape(r[:url])).path == '/news'}
-    end
-    
+  def self.remove_generic_urls!(results)
+    results.reject{|r| URI.parse(URI.escape(r[:url])).path == '/news/' or URI.parse(URI.escape(r[:url])).path == '/news'}
   end
 end
