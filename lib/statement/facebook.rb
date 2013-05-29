556af40084ef3304f00dbba8e3a07ee22703fb15
Derek Willis <dwillis@gmail.com>
2013-05-29 19:28:41 -0400

    restrict results to posts from passed-in id

--- lib/statement/facebook.rb
+++ lib/statement/facebook.rb
@@ -16,14 +16,28 @@ module Statement
     end
     
     def feed(member_id)
-      graph.get_connection(member_id, 'feed')
+      results = graph.get_connection(member_id, 'feed')
+      process_results(results.select{|r| r['from']['id'] == r['link']['id'].split('_').first})
     end
     
-    # given an array of congressional facebook ids, pulls feeds in batches of 50.
+    # given an array of congressional facebook ids, pulls feeds in slices.
     def batch(member_ids, slice)
       results = []
       member_ids.each_slice(slice) do |members|
-        results << graph.batch {|batch_api| members.each {|member| batch_api.get_connection(member, 'feed')}}
+         results << graph.batch do |batch_api| 
+          members.each do |member|
+            batch_api.get_connection(member, 'feed')
+          end
+        end
+      end
+      process_results(results.flatten.select{|r| r['from']['id'] == r['id'].split('_').first})
+    end
+    
+    def process_results(links)
+      results = []
+      links.each do |link|
+        facebook_id = link['id'].split('_').first
+        results << { :id => link['id'], :body => link['message'], :link => link['link'], :title => link['name'], :type => link['type'], :status_type => link['status_type'], :created_time => DateTime.parse(link['created_time']), :updated_time => DateTime.parse(link['updated_time']), :facebook_id => facebook_id }
       end
       results
     end
