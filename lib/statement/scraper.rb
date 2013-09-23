f81795e8a15dada8ae212cbb9aae3d23b6e6d33e
Derek Willis <dwillis@gmail.com>
2013-09-23 11:15:55 -0400

    added markey to freshman senators scraper

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -307,7 +307,7 @@ module Statement
     
     def self.freshman_senators
       results = []
-      ['murphy','cruz'].each do |senator|
+      ['markey', 'murphy','cruz'].each do |senator|
         base_url = "http://www.#{senator}.senate.gov/"
         doc = open_html(base_url+'press.cfm?maxrows=200&startrow=1&&type=1')
         return if doc.nil?
