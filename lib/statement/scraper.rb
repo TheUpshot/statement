2570b8c200993d488d384a21453b238613fcc1c3
Derek Willis <dwillis@nytimes.com>
2015-01-05 19:52:44 -0500

    added crenshaw member scraper

--- lib/statement/scraper.rb
+++ lib/statement/scraper.rb
@@ -29,7 +29,7 @@ module Statement
     end
 
     def self.member_methods
-      [:capuano, :cold_fusion, :conaway, :chabot, :susandavis, :freshman_senators, :klobuchar, :billnelson, :lautenberg, :crapo, :coburn, :boxer, :vitter, :donnelly, :inhofe, :palazzo, :roe, :document_query, :swalwell, :fischer, :clark, :edwards, :culberson_chabot_grisham, :barton, :wolf_sherman_mccaul, :welch, :sessions, :gabbard, :ellison, :costa, :farr, :mcclintock, :mcnerney, :olson]
+      [:crenshaw, :capuano, :cold_fusion, :conaway, :chabot, :susandavis, :freshman_senators, :klobuchar, :billnelson, :lautenberg, :crapo, :coburn, :boxer, :vitter, :donnelly, :inhofe, :palazzo, :roe, :document_query, :swalwell, :fischer, :clark, :edwards, :culberson_chabot_grisham, :barton, :wolf_sherman_mccaul, :welch, :sessions, :gabbard, :ellison, :costa, :farr, :mcclintock, :mcnerney, :olson]
     end
 
     def self.committee_methods
@@ -38,7 +38,7 @@ module Statement
 
     def self.member_scrapers
       year = Date.today.year
-      results = [capuano, cold_fusion(year, 0), conaway, chabot, susandavis, klobuchar(year), palazzo(page=1), roe(page=1), billnelson(year=year),
+      results = [crenshaw, capuano, cold_fusion(year, 0), conaway, chabot, susandavis, klobuchar(year), palazzo(page=1), roe(page=1), billnelson(year=year),
         document_query(page=1), document_query(page=2), swalwell(page=1), donnelly(year=year), crapo, coburn, boxer(start=1),
         vitter(year=year), inhofe(year=year), fischer, clark(year=year), edwards, culberson_chabot_grisham(page=1), barton, wolf_sherman_mccaul, welch,
         sessions(year=year), gabbard, pryor, ellison(page=0), costa, farr, mcclintock, olson, mcnerney].flatten
@@ -259,16 +259,43 @@ module Statement
       return results[0..-5]
     end
 
-    def self.cold_fusion(year=Date.today.year, month=0)
+    def self.crenshaw(year=Date.today.year, month=nil)
       results = []
       year = Date.today.year if not year
-      month = 0 if not month
-      domains = ['crenshaw.house.gov', 'www.ronjohnson.senate.gov/public/','www.risch.senate.gov/public/']
+      domain = 'crenshaw.house.gov'
+      if month
+        url = "http://crenshaw.house.gov/index.cfm/pressreleases?YearDisplay=#{year}&MonthDisplay=#{month}&page=1"
+      else
+        url = "http://crenshaw.house.gov/index.cfm/pressreleases"
+      end
+      doc = Statement::Scraper.open_html(url)
+      return if doc.nil?
+      doc.xpath("//tr")[2..-1].each do |row|
+        date_text, title = row.children.map{|c| c.text.strip}.reject{|c| c.empty?}
+        next if date_text == 'Date' or date_text.size > 10
+        date = Date.parse(date_text)
+        results << { :source => url, :url => row.children[3].children.first['href'], :title => title, :date => date, :domain => domain }
+      end
+      results
+    end
+
+    def self.cold_fusion(year=Date.today.year, month=nil)
+      results = []
+      year = Date.today.year if not year
+      domains = ['www.ronjohnson.senate.gov/public/','www.risch.senate.gov/public/']
       domains.each do |domain|
-        if domain == 'crenshaw.house.gov' or domain == 'www.risch.senate.gov/public/'
-          url = "http://"+domain + "/index.cfm/pressreleases?YearDisplay=#{year}&MonthDisplay=#{month}&page=1"
+        if domain == 'www.risch.senate.gov/public/'
+          if not month
+            url = "http://www.risch.senate.gov/public/index.cfm/pressreleases"
+          else
+            url = "http://www.risch.senate.gov/public/index.cfm/pressreleases?YearDisplay=#{year}&MonthDisplay=#{month}&page=1"
+          end
         else
-          url = "http://"+domain + "index.cfm/press-releases?YearDisplay=#{year}&MonthDisplay=#{month}&page=1"
+          if not month
+            url = "http://www.ronjohnson.senate.gov/public/index.cfm/press-releases"
+          else
+            url = "http://www.ronjohnson.senate.gov/public/index.cfm/press-releases?YearDisplay=#{year}&MonthDisplay=#{month}&page=1"
+          end
         end
         doc = Statement::Scraper.open_html(url)
         return if doc.nil?
@@ -702,6 +729,15 @@ module Statement
       doc.css("ul li[@class='article articleright']").each do |row|
         results << {:source => url, :url => 'http://bilirakis.house.gov' + row.children[3].children[1]['href'], :title => row.children[3].text.strip, :date => Date.parse(row.children[5].text), :domain => domain }
       end
+    end
+
+    def self.backfill_boustany
+      results = []
+      domain = 'boustany.house.gov'
+      url = 'http://boustany.house.gov/113th-congress/showallitems/'
+      doc = open_html(url)
+      return if doc.nil?
+
     end
 
   end
