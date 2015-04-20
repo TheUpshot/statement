c4365423552ddc610a8fea80c05949216ede5b99
Tyler Pearson <ty.pearson@gmail.com>
2015-04-19 18:31:46 -0700

    Add Ed Perlmutter press page
    Issue #63

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -10,21 +10,21 @@ describe Statement do
     @results = Feed.from_rss(@feed_url)
     @results.first[:domain].must_equal "ruiz.house.gov"
   end
-  
+
   it "parses House GOP press release page" do
     @feed_url = "http://www.gop.gov/republicans/news?offset=03/29/13"
     WebMock.stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "house_gop_releases.html")), :status => 200)
     @results = Scraper.house_gop(@feed_url)
     @results.first[:source].must_equal @feed_url
   end
-  
+
   it "does not attempt to parse dates when none are present" do
     @feed_url = "http://culberson.house.gov/feed/rss/"
     WebMock.stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "culberson_rss.xml")), :status => 200)
     @results = Feed.from_rss(@feed_url)
     @results.first[:date].must_equal nil
   end
-  
+
   it "parses invalid RSS" do
     @feed_url = "http://www.burr.senate.gov/public/index.cfm?FuseAction=RSS.Feed"
     WebMock.stub_request(:any, @feed_url).to_return(:body => File.new(File.join(File.dirname(__FILE__), "richard_burr.xml")), :status => 200)
@@ -39,28 +39,28 @@ describe Statement do
     @results = Scraper.house_gop(@feed_url)
     @results.last[:url].must_equal "http://www.gop.gov/republicans/other/relative_url_test.html"
   end
-  
+
   it "scrapes a senate cold fusion page" do
     @url = "http://www.billnelson.senate.gov/news/media.cfm?year=2013"
     WebMock.stub_request(:any, @url).to_return(:body => File.new(File.join(File.dirname(__FILE__), 'bill_nelson_press.html')), :status => 200)
     @results = Scraper.billnelson(year=2013)
     @results.last[:url].must_equal "http://www.billnelson.senate.gov/news/details.cfm?id=338190&"
   end
-  
+
   it "scrapes vitter pages for 2013" do
     @vitter = "http://www.vitter.senate.gov/newsroom/press?year=2013"
     WebMock.stub_request(:any, @vitter).to_return(:body => File.new(File.join(File.dirname(__FILE__), 'vitter_press.html')), :status => 200)
     @results = Scraper.vitter(year=2013)
     @results.map{|r| r[:domain]}.uniq.must_equal ["www.vitter.senate.gov"]
   end
-  
+
   it "only scrapes vitter page for 2012" do
     @vitter = "http://www.vitter.senate.gov/newsroom/press?year=2012"
     WebMock.stub_request(:any, @vitter).to_return(:body => File.new(File.join(File.dirname(__FILE__), 'vitter_press.html')), :status => 200)
     @results = Scraper.vitter(year=2012)
-    @results.map{|r| r[:domain]}.uniq.must_equal ["www.vitter.senate.gov"]    
+    @results.map{|r| r[:domain]}.uniq.must_equal ["www.vitter.senate.gov"]
   end
-  
+
   it "scrapes sanford's press page" do
     @sanford_url = "http://sanford.house.gov/media-center/press-releases?page=0"
     @sanford_page = File.new(File.join(File.dirname(__FILE__), 'sanford_press.html'))
@@ -76,6 +76,23 @@ describe Statement do
 
     @results = Scraper.sanford(0)
     @results.length.must_equal 10
+    @results.first.must_equal expected_result
+  end
+
+  it "scrapes perlmutter's press page" do
+    @perlmutter_url = "http://perlmutter.house.gov/index.php/media-center/press-releases-86821"
+    @perlmutter_page = File.new(File.join(File.dirname(__FILE__), 'ed_perlmutter_press.html'))
+    WebMock.stub_request(:any, @perlmutter_url).to_return(:body => @perlmutter_page, :status => 200)
+
+    expected_result = {
+      :source => "http://perlmutter.house.gov/index.php/media-center/press-releases-86821",
+      :url    => "http://perlmutter.house.gov/index.php/media-center/press-releases-86821/1505-polis-perlmutter-host-fed-reserve-chief-for-roundtable-with-cannabis-businesses",
+      :title  => "Polis, Perlmutter Host Fed Reserve Chief for Roundtable with Cannabis Businesses",
+      :date   => Date.parse("2015-04-10"),
+      :domain => "perlmutter.house.gov"
+    }
+
+    @results = Scraper.perlmutter
     @results.first.must_equal expected_result
   end
 end
